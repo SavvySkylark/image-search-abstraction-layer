8508fd309d54419e0239c3452aa895e27dc6e363
SavvySkylark <skylarsmithe@gmail.com>
2017-08-18 11:38:56 +0000

    client id add to server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+var clientId = '37037b1112e3bcb';
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -12,25 +14,25 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
+app.get('/dreams', function (request, response) {
   response.send(dreams);
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
+app.post('/dreams', function (request, response) {
   dreams.push(request.query.dream);
   response.sendStatus(200);
 });
 
 // Simple in-memory store for now
 var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
+  'Find and count some sheep',
+  'Climb a really tall mountain',
+  'Wash the dishes'
 ];
 
 // listen for requests :)
