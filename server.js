5a84f23072feaa8a3719803a6309912abd990fb6
SavvySkylark <skylarsmithe@gmail.com>
2017-09-23 13:45:21 -0400

    finished abstraction functionality. Returning unaltered response.

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const https = require('https');
+const querystring = require('querystring');
+
+const imgur3Endpoint = 'https://api.imgur.com/3/gallery/search/time';
+const clientId = 'Client-ID 6d3b8c258bd2240';
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -12,28 +17,53 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
+app.get('/api/imagesearch/:search', function(request, response) {
+  var offset = request.query.offset||-999;
+  request.params.search;
+  var qsObj = {
+    q: request.params.search
+  };
+  var offSetUri;
+  if (offset > 0) {
+    offSetUri = '/' + offset;
+  } else {
+    offSetUri = '';
+  }
+  
+  var imgurUri = '/3/gallery/search/time' + offSetUri + '?' + querystring.stringify(qsObj);
+  var options = {
+    hostname: 'api.imgur.com',
+    port: '443',
+    path: imgurUri,
+    method: 'GET',
+    headers: {
+      'Authorization': 'Client-ID 5d3b8c258bd2240'
+    }
+  };
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
+  console.log(JSON.stringify(options));
+  var req = https.request(options, (res) => {
+    res.on('data', (d) => {
+      response.write(d);
+    });
+    res.on('end', ()=>{
+      response.end();
+    });
+    res.on('error', (err) => {
+      console.log(err);
+    });
+  });
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  req.end();
 });
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen('9080', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
